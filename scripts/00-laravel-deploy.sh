564c2118f3cbca70fb411105689770f86e79c652
Allen Cruiz <cruizallen2@gmail.com>
2024-11-21 13:23:43 +0800

    some required files

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+echo "Deploying application..."
+echo "version: 1.0.0"
+echo "Github: AlienWolfX"
+echo ""
+
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "generating application key..."
+php artisan key:generate --show
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Building vite"
+npm install
+npm run build
+
